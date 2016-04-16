48912d757d6fea497413b0a63229b9590fb82a3d
Mario Andrés Correa <a@macool.me>
2016-04-16 12:58:04 -0500

    use factorygirl

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   gem 'byebug'
   gem 'rspec-rails', '~> 3.4'
   gem 'capybara'
+  gem 'factory_girl_rails'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Wuxi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.orm :mongoid
+    end
   end
 end
